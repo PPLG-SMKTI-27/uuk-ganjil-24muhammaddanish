17cedf539faf5644646e4362db6b6cd4121e45e0
Danish_Riftwalker911 <24_muhammaddanish@student.smkti.net>
2025-12-10 18:55:24 +0800

    Delete mvcparkir directory

--- mvcparkir/app/views/parkir/create.php
+++
@@ -1,8 +0,0 @@
-<h2>Input Kendaraan Masuk</h2>
-<form method="post">
-  <div class="mb-3"><label>Plat Nomor</label><input class="form-control" name="plat" required></div>
-  <div class="mb-3"><label>Jenis</label>
-    <select class="form-select" name="jenis"><option value="roda2">Roda 2</option><option value="roda4">Roda 4</option></select>
-  </div>
-  <button class="btn btn-primary">Simpan</button>
-</form>
--- mvcparkir/app/views/users/create.php
+++
@@ -1,9 +0,0 @@
-<h2>Tambah User</h2>
-<form method="post">
-  <div class="mb-3"><label>Username</label><input class="form-control" name="username" required></div>
-  <div class="mb-3"><label>Password</label><input class="form-control" name="password" type="password" required></div>
-  <div class="mb-3"><label>Role</label>
-    <select class="form-select" name="role"><option value="petugas">Petugas</option><option value="admin">Admin</option></select>
-  </div>
-  <button class="btn btn-primary">Simpan</button>
-</form>
