b481da4d5ff72cf18d55f348b8500aad9ccc3092
Danish_Riftwalker911 <24_muhammaddanish@student.smkti.net>
2025-12-11 11:41:09 +0800

    Delete Muhammad_Danish_Hidayat_Sistem_Parkir_Pusat_Perbelanjaan(Diperbaiki) directory

--- Muhammad_Danish_Hidayat_Sistem_Parkir_Pusat_Perbelanjaan(Diperbaiki)/aplikasi_parkir/app/models/BaseModel.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace app\models;
-use app\config\Database;
-class BaseModel {
-    protected $db;
-    public function __construct(){
-        $this->db = (new Database())->pdo;
-    }
-}
